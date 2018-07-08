08e3fdfc41efd1a1897ee190bcc7d413f37bce43
John <john@kaijugames.com>
2018-07-08 23:26:21 +0100

    I think I've got latlon the wrong way around :/

--- property_levels.py
+++ property_levels.py
@@ -4,7 +4,7 @@ import gdal
 import matplotlib.pyplot as plt
 import configparser
 import json
-import numpy
+import numpy as np
 import ogr
 
 
@@ -29,6 +29,7 @@ def extract_prices_and_coords(results):
 
 
 def create_wkt_from_coords(coords):
+    """Creates a multipoint geometry from coords"""
     points = ogr.Geometry(ogr.wkbMultiPoint)
     for coord in coords:
         point = ogr.Geometry(ogr.wkbPoint)
@@ -37,8 +38,33 @@ def create_wkt_from_coords(coords):
     return points.ExportToWkt()
 
 
-def
+def xyz_from_coords(coords):
+    """Returns three arrays (x,y and z) from coords"""
+    coords = np.stack(coords, 0)
+    x, y, z = np.split(coords, 3, axis=1)
+    x, y, z = [foo.flatten() for foo in (x, y, z)]
+    return x,y,z
 
+def raster_from_coords(coords, height, width):
+    """Returns a 2D image raster of dims height and width from coords."""
+    x,y,z = xyz_from_coords(coords)
+    x_res_conversion_factor = (max(x) - min(x))/width
+    y_res_conversion_factor = (max(y) - min(y))/height
+    x_pixels = [int((x_lat - min(x))*x_res_conversion_factor) for x_lat in x]
+    y_pixels = [int((y_lon - min(y))*y_res_conversion_factor) for y_lon in y]
+    out = np.zeros([width, height]) # Keeping it in gdal order [y, x]
+    for index, z_value in enumerate(z):
+        out[y_pixels[index], x_pixels[index]] = z[index]
+    return out
+
+
+def contours_from_coords(coords):
+    """Plots contours from coordinates"""
+    x, y, _ = xyz_from_coords(coords)
+    z = raster_from_coords(coords, 1000, 1000)
+    print(np.meshgrid(x,y))
+    plt.contour(x, y, z)
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -50,6 +76,7 @@ if __name__ == "__main__":
     results = request_area_prices(town, api_key)
     coords = extract_prices_and_coords(results)
     print(create_wkt_from_coords(coords))
+    contours_from_coords(coords)
 
 
     #OK, from the Zoopla API web interface; we have property price and lat-lon as -180 -180, 90, -90. We good.
